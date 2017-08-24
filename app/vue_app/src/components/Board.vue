107baea75c8c3c3c4afa219d203a6c0c9623085c
Karan Kumar <karankumar.world@gmail.com>
2017-08-24 19:09:45 +0530

    initial board component and slots

---
+++ app/vue_app/src/components/Board.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="board">
+    <div>
+      <board-slot v-for="i in 9">
+
+      </board-slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BoardSlot from './BoardSlot.vue'
+
+  export default {
+      props:{},
+      components : {BoardSlot}
+  }
+</script>
---
+++ app/vue_app/src/components/BoardSlot.vue
@@ -0,0 +1,9 @@
+<template>
+  <span>Box</span>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
