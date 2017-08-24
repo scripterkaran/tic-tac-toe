2af347569929a517f3518db7efd50c4a00b18318
Karan Kumar <karankumar.world@gmail.com>
2017-08-25 01:34:56 +0530

    board structure and user selection

--- app/vue_app/src/components/Board.vue
+++ app/vue_app/src/components/Board.vue
@@ -2,7 +2,9 @@
   <div class="board">
       <table>
         <tr v-for="i in 3">
-            <td v-for="i in 3"> x </td>
+            <td v-for="i in 3">
+              <board-slot :user_selection="user_selection"></board-slot>
+            </td>
         </tr>
       </table>
   </div>
@@ -13,6 +15,11 @@
 
   export default {
       props:{},
+      data(){
+          return {
+              user_selection: '0'
+          }
+      },
       components : {BoardSlot}
   }
 </script>
--- app/vue_app/src/components/BoardSlot.vue
+++ app/vue_app/src/components/BoardSlot.vue
@@ -1,9 +1,28 @@
 <template>
-  <span>Box</span>
+  <div class="slot-box" @click="markMove">
+    <span> {{text}}</span></div>
 </template>
 
 <script>
   export default {
-
+      props: ['user_selection'],
+      data(){
+          return{
+              text: ''
+          }
+      },
+      methods:{
+        markMove(){
+            this.text = this.user_selection
+        }
+      }
   }
 </script>
+
+<style scoped>
+  .slot-box{
+    border: 1px solid red;
+    min-height: 40px;
+    min-width: 40px;
+  }
+</style>
